c3b2c3520ebec36476ae083b344dc73258f0dbb1
omexapp <78252258+omexapp@users.noreply.github.com>
2021-02-02 09:48:55 +0500

    Update const.go

--- x/dex/types/const.go
+++ x/dex/types/const.go
@@ -2,5 +2,5 @@ package types
 
 const (
 	// TestTokenPairOwner defines owner of token pair, just for test
-	TestTokenPairOwner = "omexchain10q0rk5qnyag7wfvvt7rtphlw589m7frsku8qc9"
+	TestTokenPairOwner = "omexchain1xk4ap3nmphzx6lnf5l3hnmwhn0n7wsxy5t5ha6"
 )
